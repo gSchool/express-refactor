54a49bf0164b72f24770141b9fcda3177a39a75b
Jon Harlem <jtharlem@gmail.com>
2015-12-10 16:30:30 -0800

    added css, public, views folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname );
+app.set('views', 'views/' );
 app.use(express.static(__dirname));
 
 song = {
