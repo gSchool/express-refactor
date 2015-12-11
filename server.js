5843040a79e9ded29c02bd894e2ca38be42b978f
Tyler Bettilyon <teb311@gmail.com>
2015-12-11 12:21:02 -0800

    refactored

--- server.js
+++ server.js
@@ -1,53 +1,23 @@
 var express = require('express');
 var app = express();
 
+// Set the view engine && public folder
 app.set('view engine', 'ejs');
-app.set('views', __dirname );
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
-song = {
-  verse1: 'This is the song that never ends',
-  verse2: 'Yea some people started it not knowing what it was',
-  verse3: 'And they\'ll continue singing it because',
-}
+// import the routes for song
+var routeBootstrap = require('./controllers/routes');
 
+// Import the models
+var song = require('./models/song');
+
+// Register root route
 app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.get('/song/edit', function(req, res) {
-  res.render('edit');
-});
-
-app.get('/song/update', function(req, res) {
-  if (req.query.verse1 !== undefined) {
-    song.verse1 = req.query.verse1;
-    song.verse2 = req.query.verse2;
-    song.verse3 = req.query.verse3;
-  }
-  res.redirect('/');
-})
-
-app.get('/song/1', function(req, res) {
-  res.render('song', {
-    pageInfo: song.verse1,
-    nextPage: '1/2',
-  });
-});
-
-app.get('/song/1/2', function(req, res) {
-  res.render('song', {
-    pageInfo: song.verse2,
-    nextPage: '2/3',
-  });
-});
-
-app.get('/song/1/2/3', function(req, res) {
-  res.render('song', {
-    pageInfo: song.verse3,
-    nextPage: '/song/1',
-  });
-});
+// Register song app routes
+app.use('/song', routeBootstrap)
 
 var server = app.listen(3000, function () {
   var port = server.address().port;
